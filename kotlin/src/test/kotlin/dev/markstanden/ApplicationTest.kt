9b29e71a5d4ea96cd846dff46da8c37310178bbd
Mark Standen <no-reply@rundog.dev>
2022-06-23 07:37:47 +0100

    Created project, and sample.json

---
+++ kotlin/src/test/kotlin/dev/markstanden/ApplicationTest.kt
@@ -0,0 +1,23 @@
+package dev.markstanden
+
+import dev.markstanden.plugins.configureRouting
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun testRoot() =
+        testApplication {
+            application {
+                configureRouting()
+            }
+            client.get("/").apply {
+                assertEquals(HttpStatusCode.OK, status)
+                assertEquals("Hello World!", bodyAsText())
+            }
+        }
+}

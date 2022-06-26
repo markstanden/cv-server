5e75159628f73aae9afdd56fd8d1eb54727bfe32
Mark Standen <mark@markstanden.dev>
2022-06-26 07:42:56 +0100

    now reading from resources

--- src/main/kotlin/dev/markstanden/routes/HomepageRouting.kt
+++ src/main/kotlin/dev/markstanden/routes/HomepageRouting.kt
@@ -1,28 +1,29 @@
 package dev.markstanden.routes
 
+import dev.markstanden.Files.asResource
+
 import dev.markstanden.models.Cv
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
-import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
-import java.nio.file.Files
-import java.nio.file.Path
 
-val fileString: String = Files.readString(Path.of("/home/mark/Documents/Coding/cv-server/src/main/resources/static/sample.json"))
-val test = Json.decodeFromString(Cv.serializer(), fileString)
+
+val test = Json.decodeFromString(Cv.serializer(), asResource(path = "/static/sample.json")!!)
 fun Route.homepageRouting() {
-    route("/") {
-        get {
-            call.respond(
-
-                    FreeMarkerContent(
-                            "cv.ftl", mapOf(
-                            "user" to "user"
-                    )
-                    )
-            )
-        }
-    }
+	route("/") {
+		get {
+			call.respond(
+				FreeMarkerContent(
+					template = "cv.ftl",
+					model = mapOf(
+						"user" to test.user,
+						"experience" to test.experienceSection,
+						"sections" to test.sections
+					)
+				)
+			)
+		}
+	}
 }

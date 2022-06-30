c1961c412f2b0833b3865b75c660ccfac765b1cd
Mark Standen <no-reply@rundog.dev>
2022-06-30 13:37:46 +0100

    started building github request

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -3,48 +3,50 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    application
-    kotlin("jvm") version "1.7.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+	application
+	kotlin("jvm") version "1.7.0"
+	id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+	id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "dev.markstanden"
 version = "0.0.1"
 application {
-    mainClass.set("dev.markstanden.ApplicationKt")
+	mainClass.set("dev.markstanden.ApplicationKt")
 
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+	val isDevelopment: Boolean = project.ext.has("development")
+	applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
-    mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+	mavenCentral()
+	maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-compression-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-freemarker-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+	implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+	implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
+	implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
+	implementation("io.ktor:ktor-server-compression-jvm:$ktor_version")
+	implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+	implementation("io.ktor:ktor-server-freemarker-jvm:$ktor_version")
+	implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+	implementation("io.ktor:ktor-client-core:$ktor_version")
+	implementation("io.ktor:ktor-client-cio:$ktor_version")
+	implementation("ch.qos.logback:logback-classic:$logback_version")
 	implementation("io.github.cdimascio:dotenv-kotlin:6.3.1")
 	testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 tasks {
-    shadowJar {
-        archiveBaseName.set("shadow")
-        archiveClassifier.set("")
-        archiveVersion.set("")
-    }
+	shadowJar {
+		archiveBaseName.set("shadow")
+		archiveClassifier.set("")
+		archiveVersion.set("")
+	}
 
-    withType<Test>().configureEach {
-        useJUnitPlatform()
-    }
+	withType<Test>().configureEach {
+		useJUnitPlatform()
+	}
 }
--- kotlin/src/main/kotlin/dev/markstanden/routes/HomepageRouting.kt
+++ kotlin/src/main/kotlin/dev/markstanden/routes/HomepageRouting.kt
@@ -3,6 +3,9 @@ package dev.markstanden.routes
 import dev.markstanden.Files.asResource
 import dev.markstanden.models.Cv
 import io.github.cdimascio.dotenv.dotenv
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.request.*
@@ -14,7 +17,7 @@ import kotlinx.serialization.json.Json
 
 val test = Json.decodeFromString(Cv.serializer(), asResource(path = "/static/sample.json")!!)
 fun Route.homepageRouting() {
-	route("/") {
+	route("/form") {
 		get {
 			call.respond(
 				FreeMarkerContent(
@@ -47,14 +50,24 @@ fun Route.homepageRouting() {
 			)
 		}
 	}
-	route("/cv/{id}") {
+	route("/cv/{hash?}") {
 		get {
 			println("get to /cv/ received")
-			val pat = dotenv {
+			val doten = dotenv {
 				ignoreIfMissing = true
-			}.get("PAT") ?: "Env variable not found"
-			println("PAT: $pat")
-//			println(${call.(id)})
+			}
+			val repoPath = doten["REPO_PATH"] ?: "REPO_PATH env variable not set"
+			val filePath = doten["FILE_PATH"] ?: "FILE_PATH env variable not set"
+			val username = doten["USERNAME"] ?: "USERNAME env variable not set"
+			val pat = doten["PAT"] ?: "PAT env variable not set"
+			val hash = call.parameters["hash"] ?: doten["HASH"] ?: "HASH env variable not set"
+
+
+			val client = HttpClient(CIO).use {
+				val res = it.request("${repoPath}/${hash}/${filePath}")
+				println(res)
+			}
+
 		}
 	}
 

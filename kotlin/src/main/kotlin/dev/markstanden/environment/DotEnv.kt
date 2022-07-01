3cd457350d1039ca1245cb647ac1feade0beac92
Mark Standen <no-reply@rundog.dev>
2022-07-01 18:52:38 +0100

    tweaked dotenv setup to ignore missing dot env file properly

--- kotlin/src/main/kotlin/dev/markstanden/environment/DotEnv.kt
+++ kotlin/src/main/kotlin/dev/markstanden/environment/DotEnv.kt
@@ -6,7 +6,6 @@ import io.github.cdimascio.dotenv.dotenv
  * Wraps the external dependency providing environment variables
  */
 class DotEnv : FromEnvironment {
-	private val env = dotenv { ignoreIfMissing }
 	override fun get(key: EnvironmentVariables) =
-		env[key.name] ?: ""
+		dotenv { ignoreIfMissing = true }[key.name] ?: ""
 }

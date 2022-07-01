19d3c45186e39c18e5e77959f5576c9b40fdc894
Mark Standen <mark@markstanden.dev>
2022-07-01 18:00:19 +0100

    refactored environment variables

--- src/main/kotlin/dev/markstanden/routes/HomepageRouting.kt
+++ src/main/kotlin/dev/markstanden/routes/HomepageRouting.kt
@@ -1,8 +1,8 @@
 package dev.markstanden.routes
 
+import dev.markstanden.environment.getGithubVariables
 import dev.markstanden.models.Cv
 import dev.markstanden.models.GHContents
-import io.github.cdimascio.dotenv.dotenv
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -15,6 +15,8 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 private val json = Json { ignoreUnknownKeys = true }
+private val env = getGithubVariables()
+
 
 fun Route.homepageRouting() {
 
@@ -30,30 +32,23 @@ fun Route.homepageRouting() {
 
 	route("/cv/{folder}") {
 		get {
-			// Retrieve values from ENV variables
-			val env = dotenv {
-				ignoreIfMissing = true
-			}
-			val repoName = env["REPO_NAME"] ?: "REPO_PATH env variable not set"
-			val userName = env["USER_NAME"] ?: "USERNAME env variable not set"
-			val pat = env["PAT"] ?: "PAT env variable not set"
 
 			val folder = call.parameters["folder"]
 
-			val url = "https://api.github.com/repos/$userName/$repoName/contents/$folder/cv.json"
+			val url = "https://api.github.com/repos/${env.userName}/${env.repoName}/contents/$folder/cv.json"
 
 			val client = HttpClient(CIO)
 
 			val res = client.get(url) {
 				headers["Accept"] = "application/vnd.github.v3+json"
-				headers["Authorization"] = "token $pat"
+				headers["Authorization"] = "token ${env.personalAccessToken}"
 			}
 
 			val downloadURL = json.decodeFromString<GHContents>(res.body())
 
 			val file = client.get(downloadURL.download_url) {
 				headers["Accept"] = "application/vnd.github.v3+json"
-				headers["Authorization"] = "token $pat"
+				headers["Authorization"] = "token ${env.personalAccessToken}"
 			}
 
 			val cv = json.decodeFromString<Cv>(file.body())

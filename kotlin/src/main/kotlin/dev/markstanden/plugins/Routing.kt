1a9950292dd4fa2d82f821c5472d2134f8e5d301
Mark Standen <no-reply@rundog.dev>
2022-07-16 09:21:18 +0100

    updated routing

--- kotlin/src/main/kotlin/dev/markstanden/plugins/Routing.kt
+++ kotlin/src/main/kotlin/dev/markstanden/plugins/Routing.kt
@@ -1,28 +1,28 @@
 package dev.markstanden.plugins
 
+import dev.markstanden.datastore.DataStore
 import dev.markstanden.datastore.GitHub
 import dev.markstanden.routes.cvRoute
 import dev.markstanden.routes.healthcheckRoute
+import dev.markstanden.routes.root
 import dev.markstanden.routes.sampleRoute
 import dev.markstanden.routes.staticRouting
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 
-fun Application.configureRouting() {
-
+fun Application.configureRouting(baseRoute: String, store: DataStore) {
 	routing {
-		val path = "cv"
-		healthcheckRoute()
-		sampleRoute()
-		cvRoute(path, GitHub())
+
+		// Serves static assets, such as css and sample cv
 		staticRouting()
 
-		route("/") {
-			get {
-				call.respondRedirect("/$path/full")
-			}
+		if (this.application.developmentMode) {
+			sampleRoute()
 		}
+
+		root(baseRoute = baseRoute, store = GitHub())
+		cvRoute(baseRoute = baseRoute, store = GitHub())
+		healthcheckRoute()
 	}
 }

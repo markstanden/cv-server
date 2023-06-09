da12fb2f672bb74827b541fbd03a1e6e6ea87e56
Mark Standen <no-reply@rundog.dev>
2023-06-09 17:20:17 +0100

    added local testing

--- kotlin/src/test/kotlin/dev/markstanden/ApplicationTest.kt
+++ kotlin/src/test/kotlin/dev/markstanden/ApplicationTest.kt
@@ -4,7 +4,6 @@ import dev.markstanden.datastore.GitHub
 import dev.markstanden.plugins.configureRouting
 import dev.markstanden.plugins.configureTemplating
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import org.junit.jupiter.api.Assertions.assertEquals

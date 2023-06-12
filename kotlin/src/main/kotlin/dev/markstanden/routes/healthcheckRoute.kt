204f0506705b0253b42ac2fc61944d5544bd1a09
Mark Standen <no-reply@rundog.dev>
2023-06-12 05:46:12 +0100

    Added commenting to healthcheck route

--- kotlin/src/main/kotlin/dev/markstanden/routes/healthcheckRoute.kt
+++ kotlin/src/main/kotlin/dev/markstanden/routes/healthcheckRoute.kt
@@ -6,6 +6,8 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.healthcheckRoute() {
+
+	// Healthcheck route if needed by hosting provider
 	route("/healthcheck") {
 		get {
 			call.response.status(HttpStatusCode.OK)
